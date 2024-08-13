0767aa651678c971188f1ccfab24128424df4b84
eerraa <phyojinbin@gmail.com>
2024-08-14 06:24:12 +0900

    Add Tomak79S
    Add Tomak79S

---
+++ keyboards/era/sirind/tomak79s/tomak79s.c
@@ -0,0 +1,32 @@
+// Copyright 2023 Hyojin Bak (@eerraa)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "quantum.h"
+
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_indicators_user()) {
+         return false;
+    }
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(93, 0, 128, 128);
+        rgb_matrix_set_color(94, 0, 128, 128);
+        rgb_matrix_set_color(95, 0, 128, 128);
+        rgb_matrix_set_color(96, 0, 128, 128);
+        rgb_matrix_set_color(97, 0, 128, 128);
+        rgb_matrix_set_color(98, 0, 128, 128);
+        rgb_matrix_set_color(99, 0, 128, 128);
+        rgb_matrix_set_color(100, 0, 128, 128);
+        rgb_matrix_set_color(101, 0, 128, 128);
+    } else {
+        rgb_matrix_set_color(93, 0, 0, 0);
+        rgb_matrix_set_color(94, 0, 0, 0);
+        rgb_matrix_set_color(95, 0, 0, 0);
+        rgb_matrix_set_color(96, 0, 0, 0);
+        rgb_matrix_set_color(97, 0, 0, 0);
+        rgb_matrix_set_color(98, 0, 0, 0);
+        rgb_matrix_set_color(99, 0, 0, 0);
+        rgb_matrix_set_color(100, 0, 0, 0);
+        rgb_matrix_set_color(101, 0, 0, 0);
+    }
+    return true;
+}
