62c282cfb0662c052dee43ce48044f357ac0dced
era <phyojinbin@gmail.com>
2024-12-16 22:30:32 +0900

    Add 7B
    Add 7B

---
+++ keyboards/era/comm/7b/config.h
@@ -0,0 +1,12 @@
+// Copyright 2024 Hyojin Bak (@eerraa)
+// SPDX-License-Identifier: GPL-2.0-or-later
+ 
+#pragma once
+
+/* Reset */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+/* BACKLIGHT PWM */
+#define BACKLIGHT_PWM_DRIVER PWMD0
+#define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_A
